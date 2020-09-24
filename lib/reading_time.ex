e30071718f5eadad3db434b0d750c2ce631fc6cd
Ignacio Aguirrezabal <ignacio@wyeworks.com>
2020-09-23 22:13:46 -0300

    Initial commit

---
+++ lib/reading_time.ex
@@ -0,0 +1,18 @@
+defmodule ReadingTime do
+  @moduledoc """
+  Documentation for ReadingTime.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ReadingTime.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/reading_time_test.exs
@@ -0,0 +1,8 @@
+defmodule ReadingTimeTest do
+  use ExUnit.Case
+  doctest ReadingTime
+
+  test "greets the world" do
+    assert ReadingTime.hello() == :world
+  end
+end
