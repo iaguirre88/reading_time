6db2d83a3b4a15e4d2e463e77a74e27129e58c56
Ignacio Aguirrezabal <ignacio@wyeworks.com>
2020-10-02 22:11:11 -0300

    Bump version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ReadingTime.MixProject do
   def project do
     [
       app: :reading_time,
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       description: description(),
